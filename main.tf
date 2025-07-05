0e0763696e490cccffb6242f28246012b4d51a8a
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-07-05 15:12:16 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+ backend "s3" {
+    bucket         = "my-eks-shailesh"         # 🔁 S3 bucket name (must exist)
+    key            = "eks-cluster/terraform.tfstate"  # 📄 path to tfstate file inside the bucket
+    region         = "us-west-2"                      # 🌍 AWS region
+    dynamodb_table = "terraform-lock-table"           # 🔒 DynamoDB table for state locking
+    encrypt        = true
+}
+}
+
+
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "ram"
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.name_prefix}-vpc"
+  }
+}
